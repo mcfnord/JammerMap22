1e46a6fa96574ab427e8c4410b0829eb2b2d5daa
jrd <gh22.x.jrd@xoxy.net>
2022-10-02 17:45:29 -0700

    Just place everyone, then consider some random spot that might be better.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,62 @@
 ﻿using System.Drawing;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 
 internal class Program
 {
+
+    static bool NobodyThere(List<PlacedName> placedNames, Rectangle proposed)
+    {
+        return true;
+    }
+
+
+    static double CostInDistanceFromFriends(List<PlacedName> placed, Rectangle option)
+    {
+        // add up the pixel-seconds from option to each placed,
+        // and then average it.
+
+        foreach (var p in placed)
+        {
+        }
+
+        
+
+
+        return 0.0;
+
+    }
+
+
+    static int RandomXOrY()
+    {
+        int LOWEST = int.MinValue + 1000;
+        int HIGHEST = int.MaxValue - 1000;
+
+        Random rnd = new Random();
+    AGAIN:
+        int v = rnd.Next() - (int.MaxValue / 2);
+        if (v < LOWEST || v > HIGHEST)
+            goto AGAIN;
+
+        return v;
+    }
+
+
+
+    static Rectangle RandomlyPlacedRect(Size s)
+    {
+        int x = RandomXOrY();
+        int y = RandomXOrY();
+
+        return new Rectangle(x - s.Width / 2,
+                                y - s.Height / 2,
+                                s.Width,
+                                s.Height);
+    }
+
+
     private static void Main(string[] args)
     {
         const string TIME_TOGETHER = "timeTogether.json";
@@ -77,75 +130,104 @@ internal class Program
             // For now, just assume 200px x 50px
             Size size = new Size(200, 50); // https://github.com/mono/SkiaSharp
 
-            // 1. find 3 places where this could fit.
-            List<Rectangle> possible = new List<Rectangle>();
-            while(possible.Count < 3)
+            Rectangle rect;
+            do
             {
-                // pick a random center spot
-                // anything in integer range, minus 1000px in
-                int LOWEST = int.MinValue + 1000;
-                int HIGHEST = int.MaxValue - 1000;
-                
-                Random rnd = new Random();
-                AGAIN_X:
-                int x = rnd.Next();
-                if (x < LOWEST || x > HIGHEST)
-                    goto AGAIN_X;
-                AGAIN_Y:
-                int y = rnd.Next();
-                if (y < LOWEST || y > HIGHEST)
-                    goto AGAIN_Y;
-
-                Rectangle proposed = new Rectangle(
-                        x - size.Width / 2,
-                        y - size.Height / 2,
-                        size.Width,
-                        size.Height);
-
-                // if it's unoccupied, then it's possible.
-                if(NobodyThere(placedNames, proposed))
-                {
-                    var newGuy = new PlacedName(name, humanGuid, proposed, rnd.Next() % 360);
-                    placedNames.Add(newGuy);
-                }
+                rect = RandomlyPlacedRect(size);
             }
+            while (false == NobodyThere(placedNames, rect));
 
-            // 2. choose the place that pushes the fewest seconds far
-            // weight = seconds * pixels ? a floating point? an int64!
-            Rectangle? topPick = null;
-            foreach(var option in possible)
-            {
-                if (topPick == null)
-                    topPick = option;
-                // for this option, what's the spot weight?
-                Int64 spotWeight = 0;
-                foreach(var placed in placedNames)
-                {
-                    // add to spotWeight the seconds * pixels between tehse two
-                    xxx
-
-                }
-
-
-
-
-            }
-            // 3. with additional gravitation toward 0.0?
-            // 4. shuffle forward and back in the list a little.
+            placedNames.Add(new PlacedName("foo", "guid", rect, 128));
         }
+
+        // All have been placed somewhere. Now iterate everyone toward their highest time
+        // by making it costliest to move far away from the highest timers.
+        //for (int i = 0; i < allWhoWillAppear.Count; i++)
+        bool fMovedOne = false;
+        do
+        {
+            fMovedOne = false;
+            foreach (var one in placedNames)
+            {
+                // for each iteration, just compare current pos against a random pos.
+                // if it's not better, then just leave it.
+                // if it's better, then migrate it.
+
+                Rectangle newpossiblerect;
+                do
+                {
+                    newpossiblerect = RandomlyPlacedRect(one.Rect.Size);
+                }
+                while (false == NobodyThere(placedNames, newpossiblerect));
+
+                if (CostInDistanceFromFriends(placedNames, newpossiblerect) > CostInDistanceFromFriends(placedNames, one.Rect))
+                {
+                    one.Rect = newpossiblerect;
+                    fMovedOne = true;
+                }
+            }
+        }
+        while (fMovedOne); // iterate until we don't find a better option for anyone.
     }
 }
+
+
+
+
+            // A migrated item is re-colored based on their relationship with neighbor colors
+            // from furthest neighbor to nearest.
+            // (go ahead and skip to the last 10%)
+
+                // if it's unoccupied, then it's possible.
+//                if () { possible.Add(proposed); }
+  //          }
+
+            // I have possibles. Pick the one closest to top friends, and 
+            // 2. choose the place that pushes the fewest seconds far
+            // weight = seconds * pixels ? 
+            /*
+            Rectangle? topPick = null;
+            double goodnessOfTopPick = 0.0;
+            foreach (var option in possible)
+            {
+                if (topPick == null)
+                {
+                    topPick = option;
+                    goodnessOfTopPick = CalcGoodness(placedNames, option);
+                    continue;
+                }
+
+                var goodnessOfThisPick = CalcGoodness(placedNames, option);
+                if (goodnessOfThisPick < goodnessOfTopPick)
+                {
+                    topPick = option;
+                    goodnessOfTopPick = goodnessOfThisPick;
+                }
+            }
+            */
+
+            // for this option, what's the spot weight?
+            //Int64 spotWeight = 0;
+            //foreach(var placed in placedNames)
+            // add to spotWeight the seconds * pixels between tehse two
+            //                    var newGuy = new PlacedName(name, humanGuid, proposed, rnd.Next() % 360);
+            //                    placedNames.Add(newGuy);
+            // 3. with additional gravitation toward 0.0?
+            // 4. shuffle forward and back in the list a little.
+
+ 
 
 class PlacedName
 {
     public PlacedName(string name, string guid, Rectangle rect, int col)
     {
         Name = name;
-        Guid = guid;
+        Guidval = guid;
         Rect = rect;
         Color = col;
     }
     public string Name { get; set; }
+    public string Guidval { get; set; }
     public Rectangle Rect { get; set; }
     public int Color { get; set; }
 };
